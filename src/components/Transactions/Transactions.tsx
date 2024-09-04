27f6a608e6dd9ba68a62093f567d6e843e18e134
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-09-04 20:32:51 -0300

    Creating transaction register

--- src/components/Transactions/Transactions.tsx
+++ src/components/Transactions/Transactions.tsx
@@ -2,9 +2,7 @@ import styles from './Transactions.module.scss'
 
 // Components
 import SummaryTitle from '../SummaryTitle/SummaryTitle'
-
-// Images
-import ProfilePicture_1 from '../../assets/images/avatars/emma-richardson.jpg'
+import TransactionRegister from '../TransactionRegister/TransactionRegister'
 
 const Transactions = () => {
     return (
@@ -12,22 +10,7 @@ const Transactions = () => {
             <SummaryTitle title={'Transactions'} redirect={'View All'} />
 
             <div className={styles.transactions_box}>
-                <div className={styles.transaction}>
-                    <div className={styles.profile}>
-                        <img src={ProfilePicture_1} alt="" />
-                        <h2>Emma Richardson</h2>
-                    </div>
-
-                    <div className={styles.details}>
-                        <div className={styles.balance}>
-                            <h3>+$75.50</h3>
-                        </div>
-
-                        <div className={styles.date}>
-                            <h4>19 Aug 2024</h4>
-                        </div>
-                    </div>
-                </div>
+                <TransactionRegister name={'Emma Richardson'} balance={75.50} date={'19 Aug 2024'} />
 
                 <div className={styles.divisor}></div>
             </div>
