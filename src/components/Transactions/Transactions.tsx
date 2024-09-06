8b901a9fca673d716d0b695ecbe05f16a6b0d625
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-09-06 17:56:59 -0300

    Updating transactions map

--- src/components/Transactions/Transactions.tsx
+++ src/components/Transactions/Transactions.tsx
@@ -25,31 +25,17 @@ const Transactions = ({ data }: Props) => {
             <div className={styles.transactions_box}>
                 {
                     data.map((transaction, index) => (
-                        <TransactionRegister key={index}
-                            name={transaction.name}
-                            amount={transaction.amount}
-                            date={transaction.date.slice(0, 10)}
-                            avatar={transaction.avatar}
-                        />
+                        <div>
+                            <TransactionRegister key={index}
+                                name={transaction.name}
+                                amount={transaction.amount}
+                                date={transaction.date.slice(0, 10)}
+                                avatar={transaction.avatar}
+                            />
+                            {index < data.length - 1 && <div className={styles.divisor}></div>}
+                        </div>
                     ))
                 }
-                {/* <TransactionRegister name={'Emma Richardson'} balance={75.50} date={'19 Aug 2024'} />
-
-                <div className={styles.divisor}></div>
-
-                <TransactionRegister name={'Savory Bites Bistro'} balance={-55.50} date={'19 Aug 2024'} />
-
-                <div className={styles.divisor}></div>
-
-                <TransactionRegister name={'Daniel Carter'} balance={-42.30} date={'18 Aug 2024'} />
-
-                <div className={styles.divisor}></div>
-
-                <TransactionRegister name={'Sun Park'} balance={120.00} date={'17 Aug 2024'} />
-
-                <div className={styles.divisor}></div>
-
-                <TransactionRegister name={'Urban Services Hub'} balance={-65.00} date={'17 Aug 2024'} /> */}
             </div>
         </div>
     )
