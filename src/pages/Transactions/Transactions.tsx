d7f9c291a1218d7e90d05b00dc30e850d52b94da
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-09-06 16:39:57 -0300

    Creating transactions table & pagination

--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -3,6 +3,11 @@ import styles from './Transactions.module.scss'
 // Components
 import PageContainer from '../../components/PageContainer/PageContainer'
 
+// Images
+import ProfilePicture from '../../assets/images/avatars/emma-richardson.jpg'
+import PrevArrow from '../../assets/images/icon-caret-left.svg'
+import NextArrow from '../../assets/images/icon-caret-right.svg'
+
 const Transactions = () => {
     return (
         <PageContainer title='Transactions'>
@@ -28,6 +33,74 @@ const Transactions = () => {
                                 <option value="">All Transactions</option>
                             </select>
                         </div>
+                    </div>
+                </div>
+
+                <div className={styles.table_box}>
+                    <table className={styles.table}>
+                        <thead>
+                            <tr>
+                                <th>Recipient / Sender</th>
+                                <th>Category</th>
+                                <th>Transaction Date</th>
+                                <th>Amount</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <div className={styles.profile}>
+                                        <img src={ProfilePicture} alt="" />
+                                        <h2>Emma Richardson</h2>
+                                    </div>
+                                </td>
+                                <td>General</td>
+                                <td>19 Aug 2024</td>
+                                <td>+$75.50</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div className={styles.profile}>
+                                        <img src={ProfilePicture} alt="" />
+                                        <h2>Emma Richardson</h2>
+                                    </div>
+                                </td>
+                                <td>General</td>
+                                <td>19 Aug 2024</td>
+                                <td>+$75.50</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div className={styles.profile}>
+                                        <img src={ProfilePicture} alt="" />
+                                        <h2>Emma Richardson</h2>
+                                    </div>
+                                </td>
+                                <td>General</td>
+                                <td>19 Aug 2024</td>
+                                <td>+$75.50</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div className={styles.pagination}>
+                    <div className={styles.button}>
+                        <img src={PrevArrow} alt="" />
+                        <h3>Prev</h3>
+                    </div>
+
+                    <div className={styles.pages}>
+                        <button>1</button>
+                        <button className={styles.selected}>2</button>
+                        <button>3</button>
+                        <button>4</button>
+                        <button>5</button>
+                    </div>
+
+                    <div className={styles.button}>
+                        <h3>Next</h3>
+                        <img src={NextArrow} alt="" />
                     </div>
                 </div>
             </div>
