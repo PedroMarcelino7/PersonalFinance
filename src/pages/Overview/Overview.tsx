7fec9da04939e59776f7bdf9399904edb0b2d4e9
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-09-17 14:05:22 -0300

    Creating getData function

--- src/pages/Overview/Overview.tsx
+++ src/pages/Overview/Overview.tsx
@@ -1,6 +1,6 @@
 import styles from './Overview.module.scss'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 // Components
 import Pots from '../../components/Pots/Pots'
@@ -8,12 +8,40 @@ import Transactions from '../../components/Transactions/Transactions'
 import RecurringBills from '../../components/RecurringBills/RecurringBills'
 import PageContainer from '../../components/PageContainer/PageContainer'
 
-// Data
-import data from '../../data'
+interface Balance {
+    BAL_CURRENT: number,
+    BAL_EXPENSES: number,
+    BAL_INCOME: number
+}
 
 const Overview = () => {
+    const [balance, setBalance] = useState<Balance | null>(null)
+
+    const getData = async () => {
+        try {
+            const response = await fetch(`http://localhost:3001/get/balance`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+
+            if (!response.ok) {
+                throw new Error(`Error: ${response.statusText}`);
+            }
+
+            const result = await response.json();
+            console.log(result)
+            setBalance(result[0])
+
+            console.log('Get Activities:', result);
+        } catch (err: any) {
+            console.log('Error', err)
+        }
+    }
+
     useEffect(() => {
-        console.log(data)
+        getData()
     }, [])
 
     return (
@@ -21,22 +49,22 @@ const Overview = () => {
             <div className={styles.summary_container}>
                 <div className={`${styles.summary_box} ${styles.current}`}>
                     <h5 className={styles.summary_title}>Current Balance</h5>
-                    <h4 className={styles.balance}>${data.balance.current.toFixed(2)}</h4>
+                    <h4 className={styles.balance}>${balance?.BAL_CURRENT.toFixed(2)}</h4>
                 </div>
                 <div className={styles.summary_box}>
                     <h5 className={styles.summary_title}>Income</h5>
-                    <h4 className={styles.balance}>${data.balance.income.toFixed(2)}</h4>
+                    <h4 className={styles.balance}>${balance?.BAL_INCOME.toFixed(2)}</h4>
                 </div>
                 <div className={styles.summary_box}>
                     <h5 className={styles.summary_title}>Expenses</h5>
-                    <h4 className={styles.balance}>${data.balance.expenses.toFixed(2)}</h4>
+                    <h4 className={styles.balance}>${balance?.BAL_EXPENSES.toFixed(2)}</h4>
                 </div>
             </div>
 
             <div className={styles.summary}>
                 <div className={styles.section} style={{ width: '55%' }}>
-                    <Pots data={data.pots} />
-                    <Transactions data={data.transactions} />
+                    {/* <Pots data={data.pots} /> */}
+                    {/* <Transactions data={data.transactions} /> */}
                 </div>
                 <div className={styles.section} style={{ width: '45%' }}>
                     <RecurringBills />
