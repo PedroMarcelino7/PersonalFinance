6951f81bc944017caef03e056aca47e5af470c0d
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-10-30 22:50:38 -0300

    Creating total container on recurring bills

--- src/pages/RecurringBills/RecurringBills.tsx
+++ src/pages/RecurringBills/RecurringBills.tsx
@@ -1,16 +1,28 @@
-import styles from './RecurringBills.module.scss'
+import styles from "./RecurringBills.module.scss";
 
 // Components
-import PageContainer from '../../components/PageContainer/PageContainer'
+import PageContainer from "../../components/PageContainer/PageContainer";
+
+// Images
+import BillsIcon from "../../assets/images/icon-recurring-bills.svg";
 
 const RecurringBills = () => {
-    return (
-        <PageContainer title='Recurring Bills'>
-            <div>
+  return (
+    <PageContainer title="Recurring Bills">
+      <div className={styles.recurring_bills}>
+        <div className={styles.left_container}>
+          <div className={styles.total_container}>
+            <img src={BillsIcon} alt="" />
 
+            <div className={styles.total_description}>
+              <h3>Total Bills</h3>
+              <h2>$384.98</h2>
             </div>
-        </PageContainer>
-    )
-}
+          </div>
+        </div>
+      </div>
+    </PageContainer>
+  );
+};
 
-export default RecurringBills
+export default RecurringBills;
