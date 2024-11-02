403616e7a446841b570d623888ef46ce5edb74f8
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-11-02 18:45:52 -0300

    Updating register page

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,62 +1,79 @@
-import styles from './Register.module.scss'
+import styles from "./Register.module.scss";
+
+// Component
+import LabeledInput from "../../components/Inputs/LabeledInput/LabeledInput";
 
 // Images
-import Logo from '../../assets/images/logo-large.svg'
+import Logo from "../../assets/images/logo-large.svg";
+import EyeIcon from "../../assets/images/icon-show-password.svg";
+import EyeSlashedIcon from "../../assets/images/icon-hide-password.svg";
+import { useState } from "react";
 
 const Register = () => {
-    return (
-        <div className={styles.authentication_container}>
-            <div className={styles.image_container}>
-                <div className={styles.image_background}>
-                    <image className={styles.image_box}>
-                        <div>
-                            <img className={styles.logo} src={Logo} alt="" />
-                        </div>
+  const [showPassword, setShowPassword] = useState<boolean>(false);
 
-                        <div className={styles.image_footer}>
-                            <h2>
-                                Keep track of your money <br />
-                                and save for your future
-                            </h2>
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
-                            <p>
-                                Personal finance app puts you in control of your spending. <br />
-                                Track transactions, set budgets, adn add to savings pots easily.
-                            </p>
-                        </div>
-                    </image>
-                </div>
+  return (
+    <div className={styles.authentication_container}>
+      <div className={styles.image_container}>
+        <div className={styles.image_background}>
+          <image className={styles.image_box}>
+            <div>
+              <img className={styles.logo} src={Logo} alt="" />
             </div>
 
-            <div className={styles.formulary}>
-                <div className={styles.formulary_box}>
-                    <h1>Sign Up</h1>
+            <div className={styles.image_footer}>
+              <h2>
+                Keep track of your money <br />
+                and save for your future
+              </h2>
 
-                    <form>
-                        <div className={styles.input_box}>
-                            <label htmlFor="">Name</label>
-                            <input type="text" />
-                        </div>
-
-                        <div className={styles.input_box}>
-                            <label htmlFor="">Email</label>
-                            <input type="email" />
-                        </div>
-
-                        <div className={styles.input_box}>
-                            <label htmlFor="">Create Password</label>
-                            <input type="password" />
-                            <span>Password must be at least 8 characters.</span>
-                        </div>
-
-                        <button type="submit">Create Account</button>
-                    </form>
-
-                    <h3>Already have an account? <a href='/login'>Login!</a></h3>
-                </div>
+              <p>
+                Personal finance app puts you in control of your spending.{" "}
+                <br />
+                Track transactions, set budgets, adn add to savings pots easily.
+              </p>
             </div>
+          </image>
         </div>
-    )
-}
+      </div>
 
-export default Register
+      <div className={styles.formulary}>
+        <div className={styles.formulary_box}>
+          <h1>Sign Up</h1>
+
+          <form>
+            <LabeledInput label="Name" type="text" name="name" />
+
+            <LabeledInput label="E-mail" type="email" name="email" />
+
+            <LabeledInput
+              label="Create Password"
+              type={showPassword ? "text" : "password"}
+              name="password"
+              icon={showPassword ? EyeSlashedIcon : EyeIcon}
+              onIconClick={handleShowPassword}
+            />
+
+            <div className={styles.input_box}>
+              <label htmlFor="">Create Password</label>
+              <input type="password" />
+              <span>Password must be at least 8 characters.</span>
+            </div>
+
+            <button type="submit">Create Account</button>
+          </form>
+
+          <h3>
+            Already have an account? <a href="/login">Login!</a>
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
