9545eeac8662e6e28f10189696497239ddf7c331
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-11-02 02:15:13 -0300

    Updating LabeledInput with icon

--- src/components/Inputs/LabeledInput/LabeledInput.tsx
+++ src/components/Inputs/LabeledInput/LabeledInput.tsx
@@ -5,16 +5,24 @@ type Props = {
   type: "text" | "email" | "password";
   name: string;
   icon?: string;
+  onIconClick?: () => void;
 };
 
-const LabeledInput = ({ label, type, name, icon }: Props) => {
+const LabeledInput = ({ label, type, name, icon, onIconClick }: Props) => {
   return (
     <div className={styles.box}>
       <label htmlFor={name}>{label}</label>
 
       <div className={styles.input}>
         <input type={type} id={name} name={name} />
-        {icon && <img className={styles.icon} src={icon} alt="A" />}
+        {icon && (
+          <img
+            className={styles.icon}
+            onClick={onIconClick}
+            src={icon}
+            alt="A"
+          />
+        )}
       </div>
     </div>
   );
