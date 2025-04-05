1b3985e98c4df9ec6f65a4a00223be41f3455a39
PedroMarcelino7 <pedropmarcelino2004@gmail.com>
2025-04-05 03:11:18 -0300

    Criando páginas com React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import './App.css'
 import Sidebar from './components/sidebar/sidebar'
+import Overview from './pages/overview/overview'
+import Transactions from './pages/transactions/transactions'
+import Budgets from './pages/budgets/budgets'
+import Pots from './pages/pots/pots'
+import RecurringBills from './pages/recurringBills/recurringBills'
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div>
-      <Sidebar />
+    <div className='App'>
+      <BrowserRouter>
+        <Sidebar />
+
+        <Routes>
+          <Route path='/' element={<Overview />} />
+          <Route path='/transactions' element={<Transactions />} />
+          <Route path='/budgets' element={<Budgets />} />
+          <Route path='/pots' element={<Pots />} />
+          <Route path='/recurring-bills' element={<RecurringBills />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -11,11 +11,11 @@ import RecurringBillsIcon from '../../assets/images/icon-nav-recurring-bills.svg
 import { Container, FooterBox, LogoBox, NavigationBox, NavigationButton, TopBox } from './styles'
 
 const navigation = [
-    { icon: OverviewIcon, name: 'Overview' },
-    { icon: TransactionsIcon, name: 'Transactions' },
-    { icon: BudgetsIcon, name: 'Budgets' },
-    { icon: PotsIcon, name: 'Pots' },
-    { icon: RecurringBillsIcon, name: 'Recurring Bills' },
+    { id: 0, icon: OverviewIcon, name: 'Overview' },
+    { id: 1, icon: TransactionsIcon, name: 'Transactions' },
+    { id: 2, icon: BudgetsIcon, name: 'Budgets' },
+    { id: 3, icon: PotsIcon, name: 'Pots' },
+    { id: 4, icon: RecurringBillsIcon, name: 'Recurring Bills' },
 ]
 
 const Sidebar = () => {
@@ -28,7 +28,7 @@ const Sidebar = () => {
 
                 <NavigationBox>
                     {navigation.map((nav) => (
-                        <NavigationButton>
+                        <NavigationButton key={nav.id}>
                             <img src={nav.icon} alt="" />
 
                             <h2>{nav.name}</h2>
