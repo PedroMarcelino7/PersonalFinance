53f916d663c63dd36a8c8d0f1d6f7af04d156b34
PedroMarcelino7 <pedropmarcelino2004@gmail.com>
2025-04-12 21:49:29 -0300

    Criando layout da página de Transactions

--- src/pages/transactions/styles.jsx
+++ src/pages/transactions/styles.jsx
@@ -81,12 +81,119 @@ export const CustomSelect = styled.select`
     }
 `;
 
+export const CustomOption = styled.option`
+    padding: 1rem;
+    font-size: 1rem;
+    border-bottom: 10px solid red;
+`
+
 export const ChevronIcon = styled.img`
-  position: absolute;
-  right: 0.75rem;
-  top: 50%;
-  transform: translateY(-50%);
-  pointer-events: none;
-  width: 12px;
-  height: 12px;
+    position: absolute;
+    right: 0.75rem;
+    top: 50%;
+    transform: translateY(-50%);
+    pointer-events: none;
+    width: 12px;
+    height: 12px;
 `;
+
+export const Table = styled.table`
+    width: 100%;
+    border-collapse: collapse;
+`
+
+export const TableHeader = styled.thead`
+    border-bottom: 1px solid var(--light-gray);
+`
+
+export const TableHeaderElement = styled.th`
+    font-size: 1rem;
+    font-weight: lighter;
+    color: var(--gray);
+    text-align: start;
+    padding-block: 1rem;
+    
+    &.end {
+        text-align: end;
+    }
+`
+
+export const TableBodyRow = styled.tr`
+    border-bottom: 1px solid var(--light-gray);
+`
+
+export const TableBodyElement = styled.td`
+    padding-block: 1rem;
+    font-size: 1rem;
+    font-weight: lighter;
+    color: var(--gray);
+    text-align: start;
+
+    &.reference {
+        display: flex;
+        align-items: center;
+        gap: 0.75rem;
+
+        img {
+        width: 45px;
+        height: 45px;
+        border-radius: 50%;
+        }
+
+        h3 {
+        font-size: 1rem;
+        color: var(--dark);
+        }
+    }
+
+    &.end {
+        text-align: end;
+        font-weight: bold;
+        color: ${({ color }) => color};
+    }
+`
+
+export const TransactionsFooter = styled.div`
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+`
+
+export const NavButton = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    border: 1px solid var(--light-gray);
+    padding-block: 0.75rem;
+    padding-inline: 1.5rem;
+    border-radius: 5px;
+    cursor: pointer;
+
+    h3 {
+        font-size: 1rem;
+        font-weight: lighter;
+        color: var(--dark);
+    }
+`
+
+export const NavPages = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+
+    button {
+        border: 1px solid var(--light-gray);
+        width: 45px;
+        height: 45px;
+        border-radius: 5px;
+        cursor: pointer;
+        background-color: transparent;
+        color: var(--dark);
+        font-size: 1rem;
+
+        &.selected {
+            background-color: var(--dark);
+            color: var(--white);
+        }
+    }
+`
--- src/pages/transactions/transactions.jsx
+++ src/pages/transactions/transactions.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import PageContainer from '../../components/pageContainer/pageContainer'
-import { ChevronIcon, CustomSelect, SearchBox, SearchButton, SearchInput, SelectWrapper, SortBox, SortContainer, TransactionsContainer, TransactionsHeader } from './styles'
+import { ChevronIcon, CustomOption, CustomSelect, NavButton, NavPages, SearchBox, SearchButton, SearchInput, SelectWrapper, SortBox, SortContainer, Table, TableBodyElement, TableBodyRow, TableHeader, TableHeaderElement, TransactionsContainer, TransactionsFooter, TransactionsHeader } from './styles'
 import SearchIcon from '../../assets/images/icon-search.svg'
 import ChevronDownIcon from '../../assets/images/icon-caret-down.svg'
 
+import { useTransactions } from '../../contexts/transactionsContext'
+import { usePeople } from '../../contexts/peopleContext'
+
+import Avatar from '../../assets/images/avatars/bytewise.jpg'
+import PrevIcon from '../../assets/images/icon-caret-left.svg'
+import NextIcon from '../../assets/images/icon-caret-right.svg'
+
 const Transactions = () => {
+    const { transactions } = useTransactions()
+    const { people } = usePeople()
+
+    const getDateFormat = (transactionDate) => {
+        const date = new Date(transactionDate);
+        return date.toLocaleDateString('en-GB', {
+            day: '2-digit',
+            month: 'short',
+            year: 'numeric',
+        }).replace(',', '');
+    };
+
     return (
         <PageContainer name="Transactions">
             <TransactionsContainer>
@@ -20,7 +39,12 @@ const Transactions = () => {
 
                             <SelectWrapper>
                                 <CustomSelect>
-                                    <option value="latest">Latest</option>
+                                    <CustomOption value="1">Latest</CustomOption>
+                                    <CustomOption value="2">Oldest</CustomOption>
+                                    <CustomOption value="3">A to Z</CustomOption>
+                                    <CustomOption value="4">Z to A</CustomOption>
+                                    <CustomOption value="5">Highest</CustomOption>
+                                    <CustomOption value="6">Lowest</CustomOption>
                                 </CustomSelect>
                                 <ChevronIcon src={ChevronDownIcon} alt="chevron" />
                             </SelectWrapper>
@@ -38,6 +62,57 @@ const Transactions = () => {
                         </SortBox>
                     </SortContainer>
                 </TransactionsHeader>
+
+                <div>
+                    <Table>
+                        <TableHeader>
+                            <tr>
+                                <TableHeaderElement>Recipient / Sender</TableHeaderElement>
+                                <TableHeaderElement>Category</TableHeaderElement>
+                                <TableHeaderElement>Transaction Date</TableHeaderElement>
+                                <TableHeaderElement className='end'>Amount</TableHeaderElement>
+                            </tr>
+                        </TableHeader>
+
+                        {transactions.map((transaction, index) => (
+                            <TableBodyRow key={index}>
+                                <TableBodyElement className='reference'>
+                                    <img src={Avatar} alt="" />
+                                    <h3>{people.find((person) => person.person_id === transaction.person_id)?.person_name || ''}</h3>
+                                </TableBodyElement>
+                                <TableBodyElement>{transaction.transaction_category}</TableBodyElement>
+                                <TableBodyElement>{getDateFormat(transaction.transaction_date)}</TableBodyElement>
+                                <TableBodyElement className='end'
+                                    color={transaction.transaction_type === 'positive' ? 'var(--green)' : 'var(--red)'}
+                                >
+                                    {transaction.transaction_type === 'positive' ? '+' : '-'}${transaction.transaction_amount}
+                                </TableBodyElement>
+                            </TableBodyRow>
+                        ))}
+                    </Table>
+                </div>
+
+                <TransactionsFooter>
+                    <NavButton>
+                        <img src={PrevIcon} alt="" />
+
+                        <h3>Prev</h3>
+                    </NavButton>
+
+                    <NavPages>
+                        <button className='selected'>1</button>
+                        <button>2</button>
+                        <button>3</button>
+                        <button>4</button>
+                        <button>5</button>
+                    </NavPages>
+
+                    <NavButton>
+                        <h3>Next</h3>
+
+                        <img src={NextIcon} alt="" />
+                    </NavButton>
+                </TransactionsFooter>
             </TransactionsContainer>
         </PageContainer>
     )
