bc9bcf20990636d69b2de996584c52c9a7343aa1
PedroMarcelino7 <pedropmarcelino2004@gmail.com>
2025-08-17 15:37:39 -0300

    Criado validação e mensagem personalizada caso o usuário não tenha nenhum pot cadastrado

--- src/pages/pots/pots.jsx
+++ src/pages/pots/pots.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react'
 import PageContainer from '../../components/pageContainer/pageContainer'
-import { Button, Card, CardButtons, CardContent, CardDateBox, CardHeader, CardOptionsBox, CardOptionsContainer, CardTitle, CardTitleBox, Identifier, Option, PotsContainer, Progress, ProgressBar, ProgressBox, ProgressDescription, TotalSavedBox } from './styles'
+import { Button, Card, CardButtons, CardContent, CardDateBox, CardHeader, CardOptionsBox, CardOptionsContainer, CardTitle, CardTitleBox, EmptyPageContainer, EmptyPageTextBox, FirstPotButton, Identifier, Option, PotsContainer, Progress, ProgressBar, ProgressBox, ProgressDescription, TotalSavedBox } from './styles'
 import OptionsIcon from '../../assets/images/icon-ellipsis.svg'
 import IconLink from '../../assets/images/icon-link.svg'
 import { usePots } from '../../contexts/potsContext'
 import IconDelete from '../../assets/images/trash-solid-red.svg'
 import IconEdit from '../../assets/images/icon-edit-blue.svg'
+
+import { Link } from 'react-router';
 
 // UTILS
 import { formatCurrency } from '../../utils/formatCurrency'
@@ -41,62 +43,100 @@ const Pots = () => {
 
     return (
         <>
-            <PageContainer name="Pots" button='+ Add new Pot' onClick={() => openModal("addPot")}>
-                <PotsContainer>
-                    {pots.map((pot, index) => (
-                        <Card key={index}>
-                            <CardHeader>
-                                <CardTitleBox>
-                                    <CardTitle>
-                                        <Identifier theme={pot.theme_color} />
-                                        <h2>{pot.pot_name}</h2>
-                                    </CardTitle>
+            <PageContainer
+                name="Pots"
+                button={pots.length === 0 ? '' : '+ Add new Pot'}
+                onClick={() => openModal("addPot")}
+            >
+                {pots.length === 0
+                    ? <EmptyPageContainer>
+                        <EmptyPageTextBox>
+                            <h1>You don't have any pots created yet.</h1>
+                            <h2>Start setting aside your money.</h2>
+                        </EmptyPageTextBox>
 
-                                    <CardDateBox>
-                                        <h3>{formatDate(pot.pot_date)}</h3>
-                                    </CardDateBox>
-                                </CardTitleBox>
+                        <div>
+                            <FirstPotButton
+                                onClick={() => openModal("addPot")}
+                            >
+                                Create your first pot
+                            </FirstPotButton>
+                        </div>
+                    </EmptyPageContainer>
+                    : <PotsContainer>
+                        {pots.map((pot, index) => (
+                            <Card key={index}>
+                                <CardHeader>
+                                    <CardTitleBox>
+                                        <CardTitle>
+                                            <Identifier theme={pot.theme_color} />
+                                            <h2>{pot.pot_name}</h2>
+                                        </CardTitle>
 
-                                <CardOptionsContainer>
-                                    <img onClick={() => handleShowOptions(pot.pot_id)} src={OptionsIcon} alt="" />
+                                        <CardDateBox>
+                                            <h3>{formatDate(pot.pot_date)}</h3>
+                                        </CardDateBox>
+                                    </CardTitleBox>
 
-                                    {(showOptions !== 0 && showOptions === pot.pot_id) &&
-                                        <CardOptionsBox>
-                                            <img style={{ width: '35px' }} src={IconLink} alt="" />
-                                            <img onClick={() => openModal("edit", pot)} style={{ width: '35px' }} src={IconEdit} alt="" />
-                                            <img onClick={() => openModal("delete", pot)} style={{ width: '30px' }} src={IconDelete} alt="" />
-                                        </CardOptionsBox>
-                                    }
-                                </CardOptionsContainer>
-                            </CardHeader>
+                                    <CardOptionsContainer>
+                                        <img onClick={() => handleShowOptions(pot.pot_id)} src={OptionsIcon} alt="" />
 
-                            <CardContent>
-                                <TotalSavedBox>
-                                    <h3>Total Saved</h3>
+                                        {(showOptions !== 0 && showOptions === pot.pot_id) &&
+                                            <CardOptionsBox>
+                                                <Link
+                                                    to={pot.pot_link}
+                                                    target='_blank'
+                                                >
+                                                    <img alt=""
+                                                        style={{ width: '35px' }}
+                                                        src={IconLink}
+                                                    />
+                                                </Link>
 
-                                    <h2>{formatCurrency(pot.pot_quantity)}</h2>
-                                </TotalSavedBox>
+                                                <img alt=""
+                                                    onClick={() => openModal("edit", pot)}
+                                                    style={{ width: '35px' }}
+                                                    src={IconEdit}
+                                                />
 
-                                <ProgressBox>
-                                    <ProgressBar>
-                                        <Progress width={getPercentage(pot)} theme={pot.theme_color} />
-                                    </ProgressBar>
-                                </ProgressBox>
+                                                <img alt=''
+                                                    onClick={() => openModal("delete", pot)}
+                                                    style={{ width: '30px' }}
+                                                    src={IconDelete}
+                                                />
+                                            </CardOptionsBox>
+                                        }
+                                    </CardOptionsContainer>
+                                </CardHeader>
 
-                                <ProgressDescription>
-                                    <h5>{getPercentage(pot)}%</h5>
+                                <CardContent>
+                                    <TotalSavedBox>
+                                        <h3>Total Saved</h3>
 
-                                    <h6>Target of {formatCurrency(pot.pot_target)}</h6>
-                                </ProgressDescription>
-                            </CardContent>
+                                        <h2>{formatCurrency(pot.pot_quantity)}</h2>
+                                    </TotalSavedBox>
 
-                            <CardButtons>
-                                <Button onClick={() => openModal("addMoney", pot)}>+ Add Money</Button>
-                                <Button onClick={() => openModal("withdraw", pot)}>Withdraw</Button>
-                            </CardButtons>
-                        </Card>
-                    ))}
-                </PotsContainer>
+                                    <ProgressBox>
+                                        <ProgressBar>
+                                            <Progress width={getPercentage(pot)} theme={pot.theme_color} />
+                                        </ProgressBar>
+                                    </ProgressBox>
+
+                                    <ProgressDescription>
+                                        <h5>{getPercentage(pot)}%</h5>
+
+                                        <h6>Target of {formatCurrency(pot.pot_target)}</h6>
+                                    </ProgressDescription>
+                                </CardContent>
+
+                                <CardButtons>
+                                    <Button onClick={() => openModal("addMoney", pot)}>+ Add Money</Button>
+                                    <Button onClick={() => openModal("withdraw", pot)}>Withdraw</Button>
+                                </CardButtons>
+                            </Card>
+                        ))}
+                    </PotsContainer>
+                }
             </PageContainer>
 
             <PotsModalManager modal={modal} onClose={closeModal} />
--- src/pages/pots/styles.jsx
+++ src/pages/pots/styles.jsx
@@ -6,6 +6,34 @@ export const PotsContainer = styled.div`
     gap: 1rem;
 `
 
+export const EmptyPageContainer = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+    `
+
+export const EmptyPageTextBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    color: var(--dark);
+
+    h1 {
+        font-size: 2.25rem;
+    }
+
+    h2 {
+        font-size: 1.5rem;
+        font-weight: normal;
+    }
+`
+
 export const Card = styled.div`
     background-color: #fff;
     padding: 1.5rem;
@@ -85,6 +113,13 @@ export const CardOptionsBox = styled.div`
 
     img {
         width: 35px;
+    }
+
+    a {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
     }
 `
 
@@ -181,5 +216,17 @@ export const Button = styled.button`
     color: var(--dark);
     font-size: 1rem;
     font-weight: bold;
+    cursor: pointer;
+`
+
+export const FirstPotButton = styled.button`
+    width: 100%;
+    background-color: var(--dark);
+    border: none;
+    border-radius: 5px;
+    padding-block: 0.75rem;
+    padding-inline: 2.5rem;
+    color: var(--white);
+    font-size: 1.25rem;
     cursor: pointer;
 `
