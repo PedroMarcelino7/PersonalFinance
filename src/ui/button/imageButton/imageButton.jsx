a3f6963cf53c13398d6fd7f95c2137114e9bac47
PedroMarcelino7 <pedropmarcelino2004@gmail.com>
2025-10-04 00:04:51 -0300

    Foi criado um botão para fazer upload de imagens

--- src/ui/button/imageButton/imageButton.jsx
+++ src/ui/button/imageButton/imageButton.jsx
@@ -1,22 +1,67 @@
-import { Container, Text } from './styles'
+import { useRef, useState } from 'react';
+
+import { Container, Image, Label, InformationLabel } from './styles'
 
 // ICONS
 import { Upload as UploadIcon } from 'lucide-react';
 
 const ImageButton = () => {
-    return (
-        <Container>
-            <Text>
-                Upload an avatar image
-            </Text>
+    const fileInputRef = useRef(null);
+    const [selectedFile, setSelectedFile] = useState('')
 
-            <UploadIcon
-                size={40}
-                color='var(--blue)'
-                strokeWidth={2.25}
-                cursor={'pointer'}
+    const handleClick = () => {
+        fileInputRef.current.click();
+    };
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+
+        if (file) {
+            setSelectedFile(file)
+        }
+    };
+
+    return (
+        <>
+            <div onClick={handleClick}>
+                {selectedFile
+                    ?
+                    <Container>
+                        <Image src={URL.createObjectURL(selectedFile)} />
+
+                        <Label>
+                            {selectedFile.name}
+                        </Label>
+                    </Container>
+
+                    :
+                    <Container>
+                        <Label>
+                            Upload an avatar image
+                        </Label>
+
+                        <UploadIcon
+                            size={40}
+                            color='var(--blue)'
+                            strokeWidth={2.25}
+                            cursor={'pointer'}
+                        />
+
+                        <InformationLabel>
+                            Suggested size: 150x150 pixels
+                        </InformationLabel>
+                    </Container>
+                }
+            </div>
+
+            <input
+                type="file"
+                accept="image/*"
+                style={{ display: "none" }}
+                ref={fileInputRef}
+                onChange={handleFileChange}
             />
-        </Container>
+        </>
     )
 }
 
--- src/ui/button/imageButton/styles.jsx
+++ src/ui/button/imageButton/styles.jsx
@@ -5,15 +5,29 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    border: 1px solid var(--blue);
+    border: 1px solid var(--blue-muted);
     border-radius: 5px;
-    background-color: #e6ebfe;
-    padding-block: 1rem;
+    background-color: #ECECFF;
+    padding: 1rem;
     gap: 1rem;
 `
 
-export const Text = styled.h1`
-    font-size: 1rem;
+export const Label = styled.h1`
+    font-size: 1.25rem;
     font-weight: normal;
     color: var(--dark);
+`
+
+export const Image = styled.img`
+    background-color: var(--white);
+    border: 1px solid var(--dark-muted);
+    border-radius: 5px;
+    width: 150px;
+    height: 150px;
+`
+
+export const InformationLabel = styled.h2`
+    font-size: 1rem;
+    font-weight: normal;
+    color: var(--dark-muted);
 `
