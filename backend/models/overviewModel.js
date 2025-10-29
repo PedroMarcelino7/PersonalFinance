9724eb7b0b5085ea03ea56276a8aab1feb974d96
PedroMarcelino7 <pedropmarcelino2004@gmail.com>
2025-10-28 23:26:30 -0300

    Foi corrigido a requisição de available balance

--- backend/models/overviewModel.js
+++ backend/models/overviewModel.js
@@ -14,15 +14,12 @@ const getAvailableBalance = (callback) => {
     db.query(`
     SELECT
     (
-        (
-        SUM(CASE WHEN transaction_type = 1 THEN transaction_amount ELSE 0 END)
-        - SUM(CASE WHEN transaction_type = 0 THEN transaction_amount ELSE 0 END)
-        )
-        - (SELECT SUM(pot_quantity) FROM pots WHERE POT_STATUS = 0)
-        - (SELECT SUM(bill_amount) FROM recurring_bills WHERE NOT bill_status = 1 AND bill_type = 0)
-        + (SELECT SUM(bill_amount) FROM recurring_bills WHERE NOT bill_status = 1 AND bill_type = 1)
-    ) AS available_balance
-    FROM transactions;
+        (COALESCE((SELECT SUM(transaction_amount) FROM transactions WHERE transaction_type = 1), 0))
+        - (COALESCE((SELECT SUM(transaction_amount) FROM transactions WHERE transaction_type = 0), 0))
+        - (COALESCE((SELECT SUM(pot_quantity) FROM pots WHERE pot_status = 0), 0))
+        - (COALESCE((SELECT SUM(bill_amount) FROM recurring_bills WHERE NOT bill_status = 1 AND bill_type = 0), 0))
+        + (COALESCE((SELECT SUM(bill_amount) FROM recurring_bills WHERE NOT bill_status = 1 AND bill_type = 1), 0))
+    ) AS available_balance;
     `, callback);
 };
 
