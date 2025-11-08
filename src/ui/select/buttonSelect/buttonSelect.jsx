f68ce891b952ee613d80baa59e328d353cebef6c
PedroMarcelino7 <pedropmarcelino2004@gmail.com>
2025-11-08 14:23:31 -0300

    Foi criado uma função para fechar as opções do select ao clicar fora da caixa de opções

--- src/ui/select/buttonSelect/buttonSelect.jsx
+++ src/ui/select/buttonSelect/buttonSelect.jsx
@@ -1,18 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Container, Label, SelectBox, Selected, Options, Option, ChevronIcon, ThemeBox, SelectWithButtonDiv, Button } from './styles'
 import { ChevronDown as ArrowIcon } from 'lucide-react';
 
 const ButtonSelect = ({ label = '', emptyLabel = 'No options to select...', value, setValue, data, item_id, item_name, hasButton = false, onButtonClick }) => {
-    const [isOpen, setIsOpen] = useState(false)
-
-    const toggleDropdown = () => setIsOpen(!isOpen)
+    const [showOptions, setShowOptions] = useState(0)
 
     const handleSelect = (item) => {
         setValue(item?.[item_id])
-        setIsOpen(false)
+
     }
 
     const selectedItem = data.find((item) => item[item_id] === value) || data[0];
+
+    useEffect(() => {
+        const handleCloseOptions = (e) => {
+            if (!e.target.closest('.dropdown-container')) {
+                setShowOptions(0)
+            }
+        }
+
+        document.addEventListener('mousedown', handleCloseOptions);
+
+        return () => {
+            document.removeEventListener('mousedown', handleCloseOptions);
+        };
+    }, [])
 
     return (
         <Container>
@@ -36,7 +48,9 @@ const ButtonSelect = ({ label = '', emptyLabel = 'No options to select...', valu
                             </ChevronIcon>
                         </Selected>
                     </SelectBox>
-                    : <SelectBox onClick={toggleDropdown}>
+                    : <SelectBox
+                        onClick={() => setShowOptions(1)}
+                    >
                         <Selected>
                             <ThemeBox>
                                 <h1>{selectedItem[item_name]}</h1>
@@ -51,8 +65,8 @@ const ButtonSelect = ({ label = '', emptyLabel = 'No options to select...', valu
                                 />
                             </ChevronIcon>
                         </Selected>
-                        {isOpen && (
-                            <Options>
+                        {showOptions !== 0 && (
+                            <Options className='dropdown-container'>
                                 {data.map((item) => (
                                     <Option key={item?.[item_id]} onClick={() => handleSelect(item)}>
                                         {item?.[item_name]}
