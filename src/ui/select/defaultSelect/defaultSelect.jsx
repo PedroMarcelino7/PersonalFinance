d7ceea1c7181b971544c0630a0f43e76aee4316d
PedroMarcelino7 <pedropmarcelino2004@gmail.com>
2025-11-08 14:25:52 -0300

    Foi criado uma função para fechar as opções do select ao clicar fora da caixa de opções

--- src/ui/select/defaultSelect/defaultSelect.jsx
+++ src/ui/select/defaultSelect/defaultSelect.jsx
@@ -1,25 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Container, Label, SelectBox, Selected, Options, Option, ChevronIcon, ThemeBox, SelectWithButtonDiv } from './styles'
 import { ChevronDown as ArrowIcon } from 'lucide-react';
 
 const DefaultSelect = ({ label = '', value, setValue, data, item_id, item_name }) => {
-    const [isOpen, setIsOpen] = useState(false)
-
-    const toggleDropdown = () => setIsOpen(!isOpen)
+    const [showOptions, setShowOptions] = useState(0)
 
     const handleSelect = (item) => {
         setValue(item?.[item_id])
-        setIsOpen(false)
     }
 
     const selectedItem = data.find((item) => item[item_id] === value) || data[0];
+
+    useEffect(() => {
+        const handleCloseOptions = (e) => {
+            if (!e.target.closest('.dropdown-container')) {
+                setShowOptions(0)
+            }
+        }
+
+        document.addEventListener('mousedown', handleCloseOptions);
+
+        return () => {
+            document.removeEventListener('mousedown', handleCloseOptions);
+        };
+    }, [])
 
     return (
         <Container>
             <Label>{label}</Label>
 
             <SelectWithButtonDiv>
-                <SelectBox onClick={toggleDropdown}>
+                <SelectBox onClick={() => setShowOptions(1)}>
                     <Selected>
                         <ThemeBox>
                             <h1>{selectedItem[item_name]}</h1>
@@ -34,8 +45,8 @@ const DefaultSelect = ({ label = '', value, setValue, data, item_id, item_name }
                             />
                         </ChevronIcon>
                     </Selected>
-                    {isOpen && (
-                        <Options>
+                    {showOptions !== 0 && (
+                        <Options className='dropdown-container'>
                             {data.map((item) => (
                                 <Option key={item?.[item_id]} onClick={() => handleSelect(item)}>
                                     {item?.[item_name]}
